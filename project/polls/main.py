e575cfb9acb6dbcf320d8a609badc12872509eeb
Rishat Sharafiev <rishatsharafiev@ya.ru>
2018-08-01 23:12:57 +0300

    Add config loader
    Signed-off-by: Rishat Sharafiev <rishatsharafiev@ya.ru>

--- project/polls/main.py
+++ project/polls/main.py
@@ -1,10 +1,29 @@
-import asyncio
+import sys
 from aiohttp import web
 from routes import setup_routes
+from settings import get_config
 
 
-loop = asyncio.get_event_loop()
-app = web.Application(loop=loop)
+async def init_app(argv=None):
 
-setup_routes(app)
-web.run_app(app, host='127.0.0.1', port=8080)
+    app = web.Application()
+
+    app['config'] = get_config(argv)
+
+    # setup views and routes
+    setup_routes(app)
+
+    return app
+
+def main(argv):
+
+    app = init_app(argv)
+
+    config = get_config(argv)
+    web.run_app(app,
+                host=config['host'],
+                port=config['port'])
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
